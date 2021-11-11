633f9dfbca1794f54c37414c5b6919a81be99d38
Dor <schreiberdor@gmail.com>
2021-11-11 11:30:29 +0200

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2,1,1,5,6,4,4,7,7};
+            Console.WriteLine("init array");
+            foreach (var item in arr)
+            {
+                Console.Write(item + ",");
+            }
+            var tmp = Sorts.CountingSort(arr, 7);
+            Console.WriteLine();
+            Console.WriteLine("after sort array");
+            foreach (var item in tmp)
+            {
+                Console.Write(item + ",");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Sorts.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp1
+{
+    static class Sorts
+    {
+        public static int[] CountingSort(int[] a, int maxNum)
+        {
+            // {2,1,1,5,6,4,4,7,7}
+
+            int[] howMenyTimesNumberExistInArray = new int[maxNum + 1];
+            int[] howMenyNumbersTheNumberIsBigger = new int[a.Length];
+            bool[] History = new bool[maxNum + 1];
+            int[] result = new int[a.Length];
+
+            for (int i = 0; i <= maxNum + 1; i++)
+                howMenyTimesNumberExistInArray[a[i]]++;
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = 0; j <= a.Length - 1; j++)
+                {
+                    if (i == j) continue;
+
+                    if (History[a[i]] == true)  break;
+
+                    if (a[i] >= a[j])
+                        howMenyNumbersTheNumberIsBigger[a[i]]++;
+                }
+
+                History[a[i]] = true;
+            }
+
+            for (int i = maxNum + 1; i >= 0; i--)
+            {
+                result[howMenyNumbersTheNumberIsBigger[a[i]]] = a[i];
+                howMenyNumbersTheNumberIsBigger[a[i]]--;
+            }
+            return result;
+        }
+    }
+}
